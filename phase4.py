4e57953d7c52f2cde7e5c85fb8c7480b12efc272
PyBitDev <guillermo.alonso.c@hotmail.com>
2023-06-02 22:00:00 +0200

    Modificados los 2

--- phase4.py
+++ phase4.py
@@ -6,8 +6,21 @@ directorio_imagenes = "C:/Users/Guill/Desktop/Proyects/Astro pi 22-23/Solo_image
 ruta_salida = "resultados.txt"  # Ruta del archivo de salida
 ruta_datos = "C:/Users/Guill/Desktop/Proyects/Astro pi 22-23/data.csv"  # Ruta del archivo CSV de datos
 
-def analizar_imagen(imagen, datos):
+def es_noche(imagen):
+    umbral_brillo = 50  # Ajusta este valor según tu criterio
+
+    imagen_gris = imagen.convert("L")
+    histograma = imagen_gris.histogram()
+
+    # Calcula el brillo promedio de la imagen
+    total_pixeles = sum(histograma)
+    brillo_promedio = sum(i * valor for i, valor in enumerate(histograma)) / total_pixeles
+
+    return brillo_promedio < umbral_brillo
+
+def analizar_imagen(imagen_path, datos, numero_imagen):
     # Convertir la imagen a escala de grises
+    imagen = Image.open(imagen_path)
     imagen_gris = imagen.convert("L")
 
     # Realizar segmentación y extracción de características
@@ -28,15 +41,19 @@ def analizar_imagen(imagen, datos):
     temperatura = datos["Temperature"]
     humedad = datos["Humidity"]
 
+    # Determinar si es noche o día
+    estado_tiempo = "noche" if es_noche(imagen_gris) else "día"
+
     # Guardar resultados en el archivo de salida
     with open(ruta_salida, "a") as archivo_salida:
-        archivo_salida.write(f"Imagen {counter}:\n")
+        archivo_salida.write(f"Imagen {numero_imagen} ({counter}):\n")
         archivo_salida.write(f"Fecha/hora: {fecha_hora}\n")
         archivo_salida.write(f"Latitud: {latitud}\n")
         archivo_salida.write(f"Longitud: {longitud}\n")
         archivo_salida.write(f"Temperatura: {temperatura}\n")
         archivo_salida.write(f"Humedad: {humedad}\n")
         archivo_salida.write(f"Promedio de vegetación: {promedio_vegetacion}\n")
+        archivo_salida.write(f"Estado del tiempo: {estado_tiempo}\n")
         archivo_salida.write("------------------------\n")
 
     # Retornar los resultados del análisis
@@ -55,12 +72,11 @@ with open(ruta_datos, "r") as archivo_csv:
 # Procesar cada imagen en la lista
 for i, nombre_imagen in enumerate(lista_imagenes):
     ruta_imagen = os.path.join(directorio_imagenes, nombre_imagen)
-    imagen = Image.open(ruta_imagen)
 
     # Obtener los datos correspondientes a la imagen actual
     datos_imagen = datos_csv[i]
 
     # Realizar análisis de la imagen
-    resultado_analisis = analizar_imagen(imagen, datos_imagen)
+    resultado_analisis = analizar_imagen(ruta_imagen, datos_imagen, i+1)
 
     print(f"Imagen {i+1} analizada. Resultado: {resultado_analisis}")
